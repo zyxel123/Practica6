ed02cb4f8fc082e1db7da51ca6eb34a4d32cc391
zyxel123 <157350188+zyxel123@users.noreply.github.com>
2024-02-19 11:05:56 +0300

    Update Program.cs

--- practica6/Program.cs
+++ practica6/Program.cs
@@ -13,7 +13,7 @@ namespace practica6
             Console.WriteLine("Введите значение x = ");
             string s = Console.ReadLine();
             double x = Convert.ToDouble(s);
-            double F = 2*Math.Cos(x);
+            double F = 5 * Math.Cos(2 * x);
             Console.WriteLine($"Для значения x = {x},F = {F}.");
         }
     }
